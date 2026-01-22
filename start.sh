4b137db9be3e52880f0c9512137a430e1de15375
Vidxeltech <vidxeltechltd@gmail.com>
2026-01-22 04:13:52 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Running collectstatic..."
-python manage.py collectstatic --noinput
-
-echo "Starting Daphne..."
-exec python -m daphne realtime_questions.asgi:application --bind 0.0.0.0 --port $PORT
